4c59bd150c16b1e1bf209a1852f1c1ee69d139cf
dcmartin <github@dcmartin.com>
2019-04-13 07:53:44 -0700

    lgtm

--- sh/checkvars.sh
+++ sh/checkvars.sh
@@ -36,7 +36,7 @@ if [ ${user_input} -gt 0 ]; then
   if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- found ${user_input} userInput variables" &> /dev/stderr; fi
   for evar in $(jq -r '.userInput?[].name' "${SERVICE_TEMPLATE}"); do 
     VAL=$(jq -r '.services[]?|select(.url=="'${SERVICE_URL}'").variables|to_entries[]?|select(.key=="'${evar}'").value' ${USERINPUT}) 
-    if [ =z "${VAL:-}" ]; then echo "--- INFO -- $0 $$ -- no value found for variable ${evar}" &> /dev/stderr; continue; fi
+    if [ -z "${VAL:-}" ]; then echo "--- INFO -- $0 $$ -- no value found for variable ${evar}" &> /dev/stderr; continue; fi
     if [ ! -z "${DEBUG:-}" ]; then echo "--- INFO -- $0 $$ -- ${evar}: ${VAL}" &> /dev/stderr; fi
     if [ -s "${evar}" ]; then 
       VAL=$(cat "${evar}")
