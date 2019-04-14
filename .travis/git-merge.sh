af585e9c2c573d8ae6ffe3306050bd34f5f7c687
dcmartin <github@dcmartin.com>
2019-04-13 21:43:35 -0700

    travis script 2

--- .travis/git-merge.sh
+++ .travis/git-merge.sh
@@ -10,15 +10,15 @@ set -o errexit
 if [ ! -z "${1}" ]; then
   TPR="${1}"
 else
-  TPR=${TRAVIS_PULL_REQUEST:-} 
+  TPR=${TRAVIS_PULL_REQUEST:-false} 
 fi
 
 if [ "${TPR}" = true ];  then
   # get repo
-  REPO_SLUG=${TRAVIS_PULL_REQUEST_SLUG}
+  REPO_SLUG=${TRAVIS_PULL_REQUEST_SLUG:-none}
   # get branch
-  BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}
-  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- pull request; branch: ${BRANCH} to branch: ${TRAVIS_BRANCH}" &> /dev/stderr; fi
+  BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-none}
+  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- pull request; branch: ${BRANCH:-none} to branch: ${TRAVIS_BRANCH:-none}" &> /dev/stderr; fi
   # test for master
   case ${BRANCH} in
     master)
@@ -32,5 +32,5 @@ if [ "${TPR}" = true ];  then
 	;;
   esac
 else
-  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- non-pull request; branch: ${TRAVIS_BRANCH}" &> /dev/stderr; fi
+  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- non-pull request; branch: ${TRAVIS_BRANCH:-none}" &> /dev/stderr; fi
 fi
