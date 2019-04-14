46e92c70b89ef4dfaddbcc5a1f4503aec496a3b3
dcmartin <github@dcmartin.com>
2019-04-13 21:33:58 -0700

    updated travis to use scripts

--- .travis/travis-setenv.sh
+++ .travis/travis-setenv.sh
@@ -10,16 +10,26 @@ set -o errexit
 # minimum
 echo ${HZN_ORG_ID} > HZN_ORG_ID
 echo ${DOCKER_NAMESPACE} > DOCKER_NAMESPACE
-if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- Horizon organization: ${HZN_ORG_ID}; Docker namespace: ${DOCKER_NAMESPACE}" &> /dev/stderr; fi
 
 # on a branch
-if [ ! -z "${TAG}" ]; then echo "${TAG}" > TAG; fi
+if [ "${TRAVIS_PULL_REQUEST:-}" = false ]; then 
+  BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}
+else
+  BRANCH=${TRAVIS_BRANCH}
+fi
+if [ ! -z "${BRANCH} ]; then
+  if [ "${BRANCH:=}" != 'master' ]; then echo "${BRANCH}" > TAG; fi
+else
+  if [ "${DEBUG:-}" = true ]; then echo "+++ WARN -- $0 $$ -- null branch" &> /dev/stderr; fi
+fi
 
-if [ ${TRAVIS_PULL_REQUEST:-} = false ]; then 
-  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- non-pull-request: BRANCH: ${TRAVIS__BRANCH}; TAG: ${TAG}" &> /dev/stderr; fi
+if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- BRANCH: ${BRANCH}; TAG: ${TAG:-none}; Horizon organization: ${HZN_ORG_ID}; Docker namespace: ${DOCKER_NAMESPACE}" &> /dev/stderr; fi
+
+if [ "${TRAVIS_PULL_REQUEST:-}" = false ]; then 
+  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- non-pull-request" &> /dev/stderr; fi
   echo ${HZN_EXCHANGE_APIKEY} > APIKEY
   echo ${PRIVATE_KEY} | base64 --decode > ${HZN_ORG_ID}.key
   echo ${PUBLIC_KEY} | base64 --decode > ${HZN_ORG_ID}.pem
 else
-  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- pull-request: BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}; TAG: ${TAG}" &> /dev/stderr; fi
+  if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- pull-request; no secrets" &> /dev/stderr; fi
 fi
