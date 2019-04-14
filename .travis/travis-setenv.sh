ac080b23e2ed91ddb03ba80aff66b3dec3b2e241
dcmartin <github@dcmartin.com>
2019-04-13 21:48:24 -0700

    travis script 3

--- .travis/travis-setenv.sh
+++ .travis/travis-setenv.sh
@@ -13,19 +13,19 @@ echo ${DOCKER_NAMESPACE} > DOCKER_NAMESPACE
 
 # on a branch
 if [ "${TRAVIS_PULL_REQUEST:-}" = false ]; then 
-  BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}
+  BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-}
 else
-  BRANCH=${TRAVIS_BRANCH}
+  BRANCH=${TRAVIS_BRANCH:-}
 fi
-if [ ! -z "${BRANCH} ]; then
-  if [ "${BRANCH:=}" != 'master' ]; then echo "${BRANCH}" > TAG; fi
+if [ ! -z "${BRANCH:-}" ]; then
+  if [ "${BRANCH}" != 'master' ]; then echo "${BRANCH}" > TAG; fi
 else
-  if [ "${DEBUG:-}" = true ]; then echo "+++ WARN -- $0 $$ -- null branch" &> /dev/stderr; fi
+  if [ "${DEBUG:-}" = true ]; then echo "+++ WARN -- $0 $$ -- empty branch" &> /dev/stderr; fi
 fi
 
 if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- BRANCH: ${BRANCH}; TAG: ${TAG:-none}; Horizon organization: ${HZN_ORG_ID}; Docker namespace: ${DOCKER_NAMESPACE}" &> /dev/stderr; fi
 
-if [ "${TRAVIS_PULL_REQUEST:-}" = false ]; then 
+if [ ${TRAVIS_PULL_REQUEST:-false} = false ]; then 
   if [ "${DEBUG:-}" = 'true' ]; then echo "--- INFO -- $0 $$ -- non-pull-request" &> /dev/stderr; fi
   echo ${HZN_EXCHANGE_APIKEY} > APIKEY
   echo ${PRIVATE_KEY} | base64 --decode > ${HZN_ORG_ID}.key
