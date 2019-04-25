2edae4f68e559e3b39eaa3d991e4c78b87ff4525
dcmartin <github@dcmartin.com>
2019-04-25 13:42:02 -0700

    working against exchange

--- hzn-setup/rootfs/usr/bin/hzn-setup-node.sh
+++ hzn-setup/rootfs/usr/bin/hzn-setup-node.sh
@@ -6,28 +6,91 @@ if [ -d '/tmpfs' ]; then TMPDIR='/tmpfs'; else TMPDIR='/tmp'; fi
 # logging
 if [ -z "${LOGTO:-}" ]; then LOGTO="${TMPDIR}/${0##*/}.log"; fi
 
+##
+## functions
+##
+
+source /usr/bin/hzn-setup-tools.sh
+
 ###
 ### hzn-setup-node.sh
 ### 
 
+## read request
 while read; do
-
   if [ -z "${REPLY:-}" ]; then continue; fi
-  if [ "${REPLY:-}" = '' ]; then continue; fi
-
-  echo "${REPLY}"
-
-  RESPONSE_FILE=$(mktemp)
-  echo '{"id":"","serial":"","mac":[""],"node":"","token":"","exchange":"","org":"","pattern":""}' > ${RESPONSE_FILE}
-  SIZ=$(wc -c "${RESPONSE_FILE}" | awk '{ print $1 }')
-  if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- output: ${RESPONSE_FILE}; size: ${SIZ}" >> ${LOGTO} 2>&1; fi
-
-  echo "HTTP/1.1 200 OK"
-  echo "Content-Type: application/json; charset=ISO-8859-1"
-  echo "Content-length: ${SIZ}" 
-  echo "Access-Control-Allow-Origin: *"
-  echo ""
-  cat "${RESPONSE_FILE}"
-  rm -f ${RESPONSE_FILE}
-
+  case "${REPLY}" in
+    POST*)
+	POST=true
+        if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- received: ${REPLY}" >> ${LOGTO} 2>&1; fi
+	continue
+	;;
+    Content-Length*)
+	if [ "${POST:-false}" = true ]; then
+	  # size being sent
+	  BYTES=$(echo "${REPLY}" | sed 's/.*: \([0-9]*\).*/\1/')
+	  # margin
+	  BYTES=$((BYTES+2))
+          if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- content length: ${BYTES}" >> ${LOGTO} 2>&1; fi
+	  break;
+        fi
+	continue
+	;;
+    GET*)
+        if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- received: ${REPLY}" >> ${LOGTO} 2>&1; fi
+	POST=false
+	break
+	;;
+    *)
+	continue
+	;;
+  esac
 done
+
+## check if handling a post
+if [ "${POST:-false}" = true ]; then
+  if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- reading ${BYTES} bytes" >> ${LOGTO} 2>&1; fi
+  ## read request
+  INPUT=$(dd count=${BYTES} bs=1 | tr '\n' ' ' | tr '\r' ' ')
+  if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- processing: ${INPUT}" >> ${LOGTO} 2>&1; fi
+  ## validate JSON
+  INPUT=$(echo "${INPUT:-null}" | jq -c '.')
+  if [ ! -z "${INPUT}" ]; then
+    if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- valid: ${INPUT}" >> ${LOGTO} 2>&1; fi
+    ## generate response
+    RESPONSE='{"exchange":"'${HZN_SETUP_EXCHANGE:-none}'","org":"'${HZN_SETUP_ORG:-none}'","pattern":"'${HZN_SETUP_PATTERN:-none}'"}'
+    ## process input
+    NODE=$(hzn_setup_process "${INPUT}")
+    ## update response
+    if [ -z "${NODE:-}" ] || [ "${NODE:-}" = 'null' ]; then
+      RESPONSE=$(echo "${RESPONSE}" | jq '.node=null|.error="not found"')
+    else
+      if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- approved node:" $(echo "${NODE}" | jq -c '.' ) >> ${LOGTO} 2>&1; fi
+      RESPONSE=$(echo "${RESPONSE}" | jq '.node='$(echo "${NODE:-null}" | jq -c '.'))
+    fi
+  fi
+fi
+
+if [ "${POST:-false}" = false ] || [ -z "${INPUT}" ]; then
+  if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- error: ${INPUT}" >> ${LOGTO} 2>&1; fi
+  ## generate error
+  RESPONSE='{"error":"POST only valid JSON"}'
+fi
+
+## iff DEBUG add input received
+if [ "${DEBUG:-}" = true ]; then RESPONSE=$(echo "${RESPONSE:-null}" | jq '.|.input='${INPUT:-null}); fi
+
+## add date
+RESPONSE=$(echo "${RESPONSE}" | jq '.|.date='$(date +%s))
+
+## calculate size
+SIZ=$(echo "${RESPONSE}" | wc -c | awk '{ print $1 }')
+if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- output size: ${SIZ}" >> ${LOGTO} 2>&1; fi
+
+## send response
+echo "HTTP/1.1 200 OK"
+echo "Content-Type: application/json; charset=ISO-8859-1"
+echo "Content-length: ${SIZ}" 
+echo "Access-Control-Allow-Origin: *"
+echo ""
+echo "${RESPONSE:-error}"
--- hzn-setup/rootfs/usr/bin/hzn-setup.sh
+++ hzn-setup/rootfs/usr/bin/hzn-setup.sh
@@ -28,10 +28,11 @@ if [ -z "${HZN_SETUP_PERIOD:-}" ]; then HZN_SETUP_PERIOD=30; echo "--- INFO -- $
 
 ## FUNCTIONS
 source /usr/bin/service-tools.sh
+source /usr/bin/hzn-setup-tools.sh
 
 ## setup response script
-if [ -z "${HZN_SETUP_SCRIPT}" ]; then HZN_SETUP_SCRIPT="hzn-setup-node.sh"; fi
-HZN_SETUP_SCRIPT=$(command -v "${HZN_SETUP_SCRIPT}.sh")
+if [ -z "${HZN_SETUP_SCRIPT:-}" ]; then HZN_SETUP_SCRIPT="hzn-setup-node.sh"; fi
+HZN_SETUP_SCRIPT=$(command -v "${HZN_SETUP_SCRIPT}")
 
 ###
 ### MAIN
@@ -42,7 +43,7 @@ hzn_init
 
 ## configure service
 #SERVICES='[{"name":"mqtt","url":"http://mqtt"}]'
-CONFIG='{"tmpdir":"'${TMPDIR}'","logto":"'${LOGTO:-}'","log_level":"'${LOG_LEVEL:-}'","debug":'${DEBUG:-true}',"org":"'${HZN_SETUP_ORG:-none}'","exchange":"'${HZN_SETUP_EXCHANGE:-none}'","pattern":"'${HZN_SETUP_PATTERN:-}'","port":'${HZN_SETUP_PORT:-0}'},"approve":"auto","vendor":"any","services":'"${SERVICES:-null}"'}'
+CONFIG='{"tmpdir":"'${TMPDIR}'","logto":"'${LOGTO:-}'","log_level":"'${LOG_LEVEL:-}'","debug":'${DEBUG:-true}',"org":"'${HZN_SETUP_ORG:-none}'","exchange":"'${HZN_SETUP_EXCHANGE:-none}'","pattern":"'${HZN_SETUP_PATTERN:-}'","port":'${HZN_SETUP_PORT:-0}',"db":"'${HZN_SETUP_DB}'","username":"'${HZN_SETUP_DB_USERNAME:-none}'","approve":"auto","vendor":"any","services":'"${SERVICES:-null}"'}'
 
 ## initialize servive
 service_init ${CONFIG}
@@ -55,17 +56,28 @@ service_update "${OUTPUT_FILE}"
 ## FOREVER
 while true; do
   DATE=$(date +%s)
-  PID=$(ps | awk '{ print $1 }' | egrep "${HZN_SETUP_SCRIPT}")
+  # report in
+  if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- checking socat; port ${HZN_SETUP_PORT:-NONE}; script: ${HZN_SETUP_SCRIPT:-NONE}" >> "${LOGTO}" 2>&1; fi
+  PID=$(ps x | egrep 'socat' | egrep "${HZN_SETUP_SCRIPT}" | awk '{ print $1 }' | head -1)
   if [ -z "${PID:-}" ]; then
+    if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- starting socat; port ${HZN_SETUP_PORT:-NONE}; script: ${HZN_SETUP_SCRIPT:-NONE}" >> "${LOGTO}" 2>&1; fi
     # start listening
-    socat TCP4-LISTEN:${HZN_SETUP_PORT},fork EXEC:hzn-setup-node.sh &
-    PID=$!
+    socat TCP4-LISTEN:${HZN_SETUP_PORT},fork EXEC:${HZN_SETUP_SCRIPT} &
+    PID=$(ps x | egrep 'socat' | egrep "${HZN_SETUP_SCRIPT}" | awk '{ print $1 }' | head -1)
+    if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- started socat; PID: ${PID}; port ${HZN_SETUP_PORT}" >> "${LOGTO}" 2>&1; fi
+  elif [ "${DEBUG:-}" = true ]; then 
+    PS=$(ps x | egrep socat | egrep "${HZN_SETUP_SCRIPT}")
+    echo "--- INFO -- $0 $$ -- found ${HZN_SETUP_SCRIPT:-NONE}: ${PS}" >> "${LOGTO}" 2>&1
   fi
 
-  if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- started socat; PID: ${PID}; port ${HZN_SETUP_PORT}" >> "${LOGTO}" 2>&1; fi
+  if [ ! -z "${PID:-}" ]; then
+    if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- socat running; PID: ${PID}; port ${HZN_SETUP_PORT}" >> "${LOGTO}" 2>&1; fi
+  else
+    if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- socat failed; PID: ${PID}" >> "${LOGTO}" 2>&1; fi
+  fi
 
   # update service
-  echo '{"date":'$(date +%s)',"pid":'${PID:-0}'}' > "${OUTPUT_FILE}"
+  echo '{"date":'$(date +%s)',"pid":'${PID:-0}',"nodes":'$(hzn_setup_exchange_nodes)'}' > "${OUTPUT_FILE}"
   service_update "${OUTPUT_FILE}"
 
   # wait for ..
